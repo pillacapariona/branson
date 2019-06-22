8f92a480d1180ccda8868d24d9cee1cf7d910441
Jenny Vega <jelusita11@gmail.com>
2019-06-22 00:17:54 -0500

    update

--- src/js/branson.js
+++ src/js/branson.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-  $("body").tooltip({ selector: '[data-toggle=tooltip]' });
-  const menu = document.querySelector('.navbar-menu');
-  const menuButton = document.querySelector('.navbar-button');
-  const xButton = document.querySelector('.navbar-menu .navbar-button');
-
-  menuButton.addEventListener('click', () => {
-  menu.classList.toggle('hiden');
-});
-
-xButton.addEventListener('click', () => {
-    menu.classList.toggle('hiden');
-});
+  $("body").tooltip({ selector: '[data-toggle=tooltip]'
+  });
 });
