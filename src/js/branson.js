b69fae9a48238c2b97a30a661f72bd7e97eb4b5f
Jenny Vega <jelusita11@gmail.com>
2019-06-26 23:25:47 -0500

    update final

--- src/js/branson.js
+++ src/js/branson.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
   $("body").tooltip({ selector: '[data-toggle=tooltip]'
   });
   $('.mdb-select').materialSelect();
+  $('.carousel').carousel({
+  interval: 2500
+  })
+});
+
+$(".js-range-slider").ionRangeSlider( {
+  skin: "round",
+  type: "double",
+  from: 100,
+  to: 500,
+  min: 0,
+  max: 500,
+  prefix: "S/."
 });
